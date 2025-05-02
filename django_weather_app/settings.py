49cbd2ec4b561e15e63bf2eefda885bed54780c1
Franxix-code <ayodelefrancis2002@gmail.com>
2025-05-01 21:02:38 -1100

    Fix ALLOWED_HOSTS for deployment

--- django_weather_app/settings.py
+++ django_weather_app/settings.py
@@ -25,7 +25,8 @@ SECRET_KEY = 'django-insecure-&wdflilv6xzp0tuy7kqg=*6%1oem#l73dp=upb(!*9ia(#akle
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['weatherwise.onrender.com', 'localhost', '127.0.0.1']
+ALLOWED_HOSTS = ['weatherwise-1-5ocw.onrender.com', 'localhost', '127.0.0.1']
+
 
 
 # Application definition
